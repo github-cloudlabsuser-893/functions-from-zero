8acdfc7cfdd5a90e0e7ee0e4d9fcd8f451597f51
Il Beppe <167537575+github-cloudlabsuser-893@users.noreply.github.com>
2024-07-15 09:43:02 +0000

    Passaggio di parametri da prompt

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
+import argparse
+
 def add(x, y):
-    return x + y
+    return int(x) + int(y)
 
 
-print(add(2, 2))
-# var=
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("parametro1")
+    parser.add_argument("parametro2")
+    args = parser.parse_args()
+    print(args.parametro1) 
+    print(args.parametro2) 
+    print(add(args.parametro1, args.parametro2))
+
